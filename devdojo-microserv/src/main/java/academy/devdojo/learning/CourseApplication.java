6f0a1be13e1f63d3be3701afedf8a11c3851a4ce
wagnerbsjunior <wagnerjf@gmail.com>
2021-02-25 17:46:39 -0300

    Discovery module changes

--- devdojo-microserv/src/main/java/academy/devdojo/learning/CourseApplication.java
+++ devdojo-microserv/src/main/java/academy/devdojo/learning/CourseApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CouseApplication {
+public class CourseApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CouseApplication.class, args);
+        SpringApplication.run(CourseApplication.class, args);
     }
 }
--- discovery/src/main/java/academey/devdojo/learning/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/academey/devdojo/learning/discovery/DiscoveryApplication.java
@@ -2,8 +2,10 @@ package academey.devdojo.learning.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 class DiscoveryApplication {
 
 	public static void main(String[] args) {
