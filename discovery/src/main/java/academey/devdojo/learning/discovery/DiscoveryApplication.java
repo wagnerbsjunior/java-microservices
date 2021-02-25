611256c9e7b09e546d3a4696575b05c92999fdc2
wagnerbsjunior <wagnerjf@gmail.com>
2021-02-25 17:17:58 -0300

    Discovery module

---
+++ discovery/src/main/java/academey/devdojo/learning/discovery/DiscoveryApplication.java
@@ -0,0 +1,13 @@
+package academey.devdojo.learning.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class DiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryApplication.class, args);
+	}
+
+}
---
+++ discovery/src/test/java/academey/devdojo/learning/discovery/DevdojoApplicationTests.java
@@ -0,0 +1,13 @@
+package academey.devdojo.learning.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevdojoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
